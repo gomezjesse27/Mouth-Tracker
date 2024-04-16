65e691f682c5955ac7f2b61c157a696467f72615
jiink <jacob.j.carlson03@gmail.com>
2024-04-15 18:29:41 -0700

    Clean up pca and regression modelling file

--- pca_and_lin_reg_modeling.py
+++ pca_and_lin_reg_modeling.py
@@ -22,12 +22,12 @@ joblib.dump(pca, 'pca_model.pkl')
 
 # Plot percentages of explained variance
 per_var = np.round(pca.explained_variance_ratio_* 100, decimals=1)
-labels = ['PC' + str(x) for x in range(1, len(per_var)+1)]
-plt.bar(x=range(1, len(per_var)+1), height=per_var, tick_label=labels)
-plt.ylim(0, 100)  # Set the y-axis limits
-plt.ylabel('Percentage of Explained Variance')
-plt.xlabel('Principal Component')
-plt.title('Scree Plot')
+# labels = ['PC' + str(x) for x in range(1, len(per_var)+1)]
+# plt.bar(x=range(1, len(per_var)+1), height=per_var, tick_label=labels)
+# plt.ylim(0, 100)  # Set the y-axis limits
+# plt.ylabel('Percentage of Explained Variance')
+# plt.xlabel('Principal Component')
+# plt.title('Scree Plot')
 #plt.show()
 print(f"Total variance explained by these {len(per_var)} components: {sum(per_var)}%")
 
@@ -36,11 +36,3 @@ model = LinearRegression()
 model.fit(X_pca, y)
 # Save the model to disk
 joblib.dump(model, 'linear_regression_model.pkl')
-
-
-# Predict the target variable for a new datapoint
-# new_datapoint = [[0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]]  # Replace with your new datapoint
-# new_datapoint_pca = pca.transform(new_datapoint)
-# prediction = model.predict(new_datapoint_pca)
-
-# print('Predicted target:', prediction)
