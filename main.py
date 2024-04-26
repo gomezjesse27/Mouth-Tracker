68e84a3c57a396cd42d79be8fc3ffdb64ca47b4a
Jacob C <jacob.j.carlson03@gmail.com>
2024-04-25 18:53:04 -0700

    Rename main file

--- main.py
+++ main.py
@@ -1,27 +1,108 @@
-"""
-Created 3/27/2024
-Main Script for CBMT Project
-"""
-import subprocess
-import os
+# It's the main deal!
+
 import sys
+import cv2
+import pygame
+import numpy as np
+import random
+import csv
+import pygame_widgets
+from pygame_widgets.button import Button
+from enum import Enum
 from config import *
+from emoji_drawing import draw_emoji
+from data_collection import data_collection_init, data_collection_update
+from data_preprocessing import data_preprocessing_update, data_preprocessing_init
+from modeling_lr import modeling_lr_init, modeling_lr_update
+from modeling_cnn import modeling_cnn_init, modeling_cnn_update
+from prediction import prediction_init, prediction_update
 
-# Path to your virtual environment's Python interpreter
-# If you don't want to use a virtual environment, you can comment out this line
-venv_python_path = os.path.join(os.getcwd(), 'venv', 'Scripts', 'python')
+WINDOW_WIDTH, WINDOW_HEIGHT = 800, 600
+FPS_LIMIT = 30
 
-def run_script(script_name):
-    print(f"Running {script_name}")
-    result = subprocess.run([venv_python_path, f"./{script_name}"])
-    if result.returncode != 0:
-        print(f"Error running {script_name}, exiting.")
-        sys.exit(1)
+# Globals -------------------------
+cap = cv2.VideoCapture(WEBCAM_ID)
 
-print_config()
-run_script("data_collection.py")
-run_script("data_preprocessing_pca.py")
-run_script("modeling_lr.py")
-run_script("prediction_lr.py")
 
-print("Goodbye!")
+# Functions -----------------------
+
+class ProgramState(Enum):
+    DATA_COLLECTION = 1
+    DATA_PREPROCESSING = 2
+    MODELING = 3
+    PREDICTION = 4
+    MAX = 5
+state = ProgramState.DATA_COLLECTION
+
+def set_state(new_state):
+    global state
+    state = new_state
+    match state:
+        case ProgramState.DATA_COLLECTION:
+            print("Data Collection")
+            data_collection_init()
+        case ProgramState.DATA_PREPROCESSING:
+            print("Data Preprocessing")
+            data_preprocessing_init()
+        case ProgramState.MODELING:
+            print("Modeling")
+            if ALGORITHM == Algorithms.LINEAR_REGRESSION:
+                modeling_lr_init()
+            else:
+                modeling_cnn_init()
+        case ProgramState.PREDICTION:
+            print("Prediction")
+            prediction_init()
+
+def main():
+    # Graphical setup ----------------------------------------------------
+    pygame.init()
+    pygame.display.set_caption('Calibration-based Mouth Tracking')
+    screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    clock = pygame.time.Clock()
+    running = True
+    set_state(ProgramState.DATA_COLLECTION)
+    # Main loop ---------------------------------------------------------------
+    while running:
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill((0, 50, 50))
+        
+        # Update based on state ------------------------   
+        match state:
+            case ProgramState.DATA_COLLECTION:
+                done = data_collection_update(screen, events, cap)
+                if done:
+                    if ALGORITHM == Algorithms.LINEAR_REGRESSION:
+                        set_state(ProgramState.DATA_PREPROCESSING)
+                    else:
+                        set_state(ProgramState.MODELING) # CNN doesnt use PCA
+            case ProgramState.DATA_PREPROCESSING:
+                done = data_preprocessing_update(screen, events)
+                if done:
+                    set_state(ProgramState.MODELING)
+                pass
+            case ProgramState.MODELING:
+                done = False
+                if ALGORITHM == Algorithms.LINEAR_REGRESSION:
+                    done = modeling_lr_update(screen, events)
+                else:
+                    done = modeling_cnn_update(screen, events, cap)
+                pass
+                if done:
+                    set_state(ProgramState.PREDICTION)
+            case ProgramState.PREDICTION:
+                prediction_update(screen, events, cap)
+                pass
+        # Between-frame stuff -----------------------
+        pygame_widgets.update(events)  # Call once every loop to allow widgets to render and listen
+        pygame.display.flip()
+        clock.tick(FPS_LIMIT)
+    
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
