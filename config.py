7aa2cabd047964c7584d1f3c38a608ab3d4ae26f
Jaysen Gomez <116235937+gomezjesse27@users.noreply.github.com>
2024-04-26 18:19:20 -0700

    Some comments on model

--- config.py
+++ config.py
@@ -10,10 +10,10 @@ RESOLUTION = 64
 ALGORITHM = Algorithms.CNN
 TARGET_COUNT = 3
 TARGET_NAMES = ['smile', 'mouth_open', 'puff', 'frown']
-FEATURE_COUNT = RESOLUTION * RESOLUTION  # Assuming grayscale images
+FEATURE_COUNT = RESOLUTION * RESOLUTION  
 BATCH_SIZE = 32  # Batch size for model training
 
-# Assuming that the label columns are always the last two columns
+
 FEATURE_COLUMNS = slice(0, FEATURE_COUNT)
 LABEL_COLUMNS = slice(-2, None)
 
